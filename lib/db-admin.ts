3d1d0a5165bcdf143a4f69f0476b0c21f7ff69f5
karimLa <kira00luffy@gmail.com>
2021-03-01 21:22:53 +0100

    moved error handling from db function to the api routes

--- lib/db-admin.ts
+++ lib/db-admin.ts
@@ -6,50 +6,38 @@ import ISite from '@/interfaces/site'
 import { db } from '@/lib/firebase-admin'
 
 type getAllFeedbackRespone = {
-	feedback: IFeedback[] | null,
-	error: FirebaseError | null
+	feedback: IFeedback[]
 }
 
 export async function getAllFeedback(siteId: string): Promise<getAllFeedbackRespone> {
-	try {
-		const snapshot = await db.collection('feedback').where('siteId', '==', siteId).get()
-		const feedback: IFeedback[] = []
+	const snapshot = await db.collection('feedback').where('siteId', '==', siteId).get()
+	const feedback: IFeedback[] = []
 
-		snapshot.forEach(doc => {
-			feedback.push({ id: doc.id, ...doc.data() } as IFeedback)
-		})
+	snapshot.forEach(doc => {
+		feedback.push({ id: doc.id, ...doc.data() } as IFeedback)
+	})
 
-		feedback.sort((a, b) => compareDesc(parseISO(a.createdAt), parseISO(b.createdAt)))
+	feedback.sort((a, b) => compareDesc(parseISO(a.createdAt), parseISO(b.createdAt)))
 
-		return { feedback, error: null }
-	} catch (error) {
-		return { error, feedback: null }
-	}
+	return { feedback }
 }
 
 type getAllSitesRespone = {
-	sites: ISite[] | null,
-	error: FirebaseError | null
+	sites: ISite[],
 }
 
 export async function getAllSites(): Promise<getAllSitesRespone> {
-	try {
-		const snapshot = await db.collection('sites').get()
-		const sites: ISite[] = []
+	const snapshot = await db.collection('sites').get()
+	const sites: ISite[] = []
 
-		snapshot.forEach(doc => {
-			sites.push({ id: doc.id, ...doc.data() } as ISite)
-		})
+	snapshot.forEach(doc => {
+		sites.push({ id: doc.id, ...doc.data() } as ISite)
+	})
 
-		return { sites, error: null }
-	} catch (error) {
-		return { error, sites: null }
-	}
+	return { sites }
 }
 
-type getUserSitesRespone = {
-	sites: ISite[]
-}
+type getUserSitesRespone = getAllSitesRespone
 
 export async function getUserSites(userId: string): Promise<getUserSitesRespone> {
 	const snapshot = await db.collection('sites').where('authorId', '==', userId).get()
